7bb27fd73208f4a40c964c4afb1f8240a62be1b1
saggam22 <saggam92@icloud.com>
2022-07-05 01:15:31 +0900

    0704 add grid

---
+++ mes_project/src/main/java/com/rezzo/mes/ccd/web/CcdController.java
@@ -0,0 +1,14 @@
+package com.rezzo.mes.ccd.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CcdController {
+	
+	@RequestMapping("/ccd.do")
+	public String ccd() {
+		return "ccd/ccd";
+	}
+
+}
