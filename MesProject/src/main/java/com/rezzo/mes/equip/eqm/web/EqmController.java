16869133f9269fa1db6c2be3a8c2228f88e6f509
Josooyeonn <101534986+Josooyeonn@users.noreply.github.com>
2022-07-11 18:15:12 +0900

    라인관리 CRUD 설비관리 - 라인목록 옵션 추가

--- MesProject/src/main/java/com/rezzo/mes/equip/eqm/web/EqmController.java
+++ MesProject/src/main/java/com/rezzo/mes/equip/eqm/web/EqmController.java
@@ -13,32 +13,40 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rezzo.mes.equip.eqm.mapper.EqmMapper;
 import com.rezzo.mes.equip.eqm.service.EqmVO;
+import com.rezzo.mes.equip.line.mapper.EqmLineMapper;
+import com.rezzo.mes.equip.line.service.EqmLineVO;
 
 @Controller
 public class EqmController {
 
-	@Autowired EqmMapper map;
+	@Autowired
+	EqmMapper map;
+	@Autowired
+	EqmLineMapper mapper;
 
 	@RequestMapping("eqm")
-	public String eqm() {
-		return "eqm/eqm";
+	public String eqmLineList (Model model){
+		List<EqmLineVO> eqmLineList = mapper.eqmLineList(null);
+		model.addAttribute("opList", eqmLineList);
+		return "equip/eqm";
 	}
-
+	
+	
 	@GetMapping("eqmList")
 	@ResponseBody
-	public List<EqmVO> EqmList(EqmVO vo) {
+	public List<EqmVO> EqmList(EqmVO vo, Model model) {
 		List<EqmVO> eqmList = map.eqmList(vo);
 		return eqmList;
 	}
 
+	@PostMapping("eqmSelect")
+	@ResponseBody
+	public List<EqmVO> eqmSelect(@RequestParam(value = "keyword") String keyword, Model model) {
+		return map.eqmSelect(keyword);
+	}
 
-	 @PostMapping("eqmSelect")
-	 public String eqmSelect(@RequestParam(value="keyword") String keyword, Model model) {
-		 model.addAttribute("selectList", map.eqmSelect(keyword));
-		 return "eqmSelect";
-	 }
-	 
-/*
+
+	/*
 	 * @RequestMapping("line") public String line() { return "eqm/line"; }
 	 * 
 	 * @RequestMapping("eqmopr") public String eqmopr() { return "eqm/eqmopr"; }
