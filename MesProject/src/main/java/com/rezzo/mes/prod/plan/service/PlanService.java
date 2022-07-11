d5c717786db70dea729c7c3c47d7cd4d72cac421
hjj <hjj3343@gmail.com>
2022-07-11 19:15:44 +0900

    0711 생산계획관리 구현중

---
+++ MesProject/src/main/java/com/rezzo/mes/prod/plan/service/PlanService.java
@@ -0,0 +1,9 @@
+package com.rezzo.mes.prod.plan.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+public interface PlanService {
+	public List<PlanVO> orderListModal(PlanVO vo);
+}
