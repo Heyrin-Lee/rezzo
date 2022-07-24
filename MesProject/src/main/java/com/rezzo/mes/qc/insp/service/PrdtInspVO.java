4e78bcb12407e600a974c0bea5ddb79a128c960b
saggam22 <saggam92@icloud.com>
2022-07-25 01:58:02 +0900

    0724 검사관리...

--- MesProject/src/main/java/com/rezzo/mes/qc/insp/service/PrdtInspVO.java
+++ MesProject/src/main/java/com/rezzo/mes/qc/insp/service/PrdtInspVO.java
@@ -1,5 +1,33 @@
 package com.rezzo.mes.qc.insp.service;
 
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class PrdtInspVO {
+	
+	String edctsLotNo;       
+	String edctsCd;  
+	String edctsNm;
+	String orderCd;
+	String inspRslt;
+	String inspPsch;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	Date inspDt;
+	
+	
+	String inspCd;	
+	String inspNm;
+	String inspSd;	
+	String inspRsltDtl;
+	String inspJm;
 
 }
--- MesProject/src/main/java/com/rezzo/mes/qc/insp/web/PrdtInspController.java
+++ MesProject/src/main/java/com/rezzo/mes/qc/insp/web/PrdtInspController.java
@@ -1,14 +1,29 @@
 package com.rezzo.mes.qc.insp.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.rezzo.mes.qc.insp.service.PrdtInspService;
+import com.rezzo.mes.qc.insp.service.PrdtInspVO;
 
 @Controller
 public class PrdtInspController {
 	
+	@Autowired PrdtInspService service;
+	
 	@RequestMapping("prdtInsp")
 	public String prdtInsp() {
 		return "qc/prdtInsp";
+	}
+	
+	@RequestMapping("getInspCode")
+	@ResponseBody
+	public List<PrdtInspVO> getInspCode() {
+		return service.getInspCode();
 	}
 
 }
