5aeeadb795583c4b69a46f14fdfbe3e0647d3da6
saggam22 <101535017+saggam22@users.noreply.github.com>
2022-07-29 10:29:48 +0900

    Delete mes_project directory

--- mes_project/src/main/java/com/rezzo/mes/order/serviceImpl/OrderServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.rezzo.mes.order.serviceImpl;
-
-public class OrderServiceImpl {
-
-}
--- mes_project/src/main/java/com/rezzo/mes/prod/prcs/mapper/PrcsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rezzo.mes.prod.prcs.mapper;
-
-import java.util.List;
-
-import com.rezzo.mes.prod.prcs.service.PrcsVO;
-
-public interface PrcsMapper {
-	public List<PrcsVO> prcsList(PrcsVO vo);
-}
